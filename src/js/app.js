730d34ecc5c6a2260699f12d2d45ab807423dc0d
stavr1708 <stavr1717@gmail.com>
2024-02-13 16:21:51 +0300

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log("Zdarova");
+// console.log("Zdarova");
+new Swiper('.testimonials-slider',{
+  navigation: {
+    prevEl:'.swiper-button-prev',
+    nextEl:'.swiper-button-next'
+  },
+  pagination:{
+    el:'.swiper-pagination',
+    clickable:true,
+    // dynamicBullets:true,
+  },
+  //отключаем перетаскивание по нажатию на картинку слайдера
+  simulateTouch:true,
+  grabCursor:true,
+  //data-hash навигация для слайдов в строке
+  hashNavigation:{
+    watchState:true,
+  },
+  autoHeight:true,
+});
